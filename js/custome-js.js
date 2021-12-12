ddf678772c32c9fdcb5462923cbe3bc03c7b8732
ashish <ashishvish0105@gmail.com>
2021-12-12 22:19:46 +0530

    upload file

---
+++ js/custome-js.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//     $("#faq_btn").click(function() {
+//         $("#info-faq").css("display", "block");
+//     });
+// });
+// $(document).ready(function() {
+//     $("h1").click(function() {
+//         $("#info-faq1").css("display", "block");
+//     });
+// });
+
+$(document).ready(function() {
+    $("#faq_btn").click(function() {
+        $("#info-faq1").toggle();
+    });
+});
